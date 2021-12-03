d8b75ff3405eed7bf946cf7124e44ca0696c270a
'ed' <'klaizen91@gmail.com'>
2021-12-04 03:39:36 +0900

    211204 001

--- web/src/main/webapp/exam01/exam.js
+++ web/src/main/webapp/exam01/exam.js
@@ -20,8 +20,6 @@ var books = [{
     bSal: 25000
 }];
 
-var bookAry = [];
-
 var fields = {
     bCode: '도서코드',
     bName: '도서명',
@@ -66,22 +64,21 @@ function submitFnc(e) {
         bName: b_Name,
         bAuthor: b_Author,
         bPub: b_Pub,
-        bSal: b_Sal
+        bSal: parseInt(b_Sal)
     }
 
     document.getElementById('tdy').appendChild(getTrBook(boo));
-
 
 }
 
 
 function createTableList() {
     var table = document.createElement('table');
-    table.setAttribute('border', '1'); /////////////속성삽입///////////////
-
+   
     //타이틀 부분 생성 
     var thead = makeHead();
     table.appendChild(thead);
+    
 
     //데이터 부분 생성
     var tbody = makeBody();
@@ -97,10 +94,12 @@ function makeHead() {
     //체크박스
     var td = document.createElement('th');
     var chkbox = document.createElement('input');
-    //chkbox.onchange = changeFnc;
     chkbox.setAttribute('type', 'checkbox');
+    chkbox.setAttribute('name', 'allChk');
+    chkbox.setAttribute('onclick', 'selectAll(this)');
     td.appendChild(chkbox);
     tr.appendChild(td);
+    
 
     for (var field in fields) {
         var td = document.createElement('th');
@@ -111,7 +110,6 @@ function makeHead() {
     td = document.createElement('th');
     td.textContent = '삭제';
     tr.appendChild(td);
-
     thead.appendChild(tr);
     return thead;
 }
@@ -129,21 +127,40 @@ function makeBody() {
 function getTrBook(book) {
     var tr = document.createElement('tr');
 
-
     //체크박스
     var td = document.createElement('th');
     var chkbox = document.createElement('input');
-    chkbox.setAttribute('name','chk');
-    //chkbox.onchange = changeFnc;
+    
     chkbox.setAttribute('type', 'checkbox');
+    chkbox.setAttribute('onclick', 'checkSelectAll(this)');
     td.appendChild(chkbox);
     tr.appendChild(td);
+
     //기본값
     for (var field in book) {
         var td = document.createElement('td');
-        td.textContent = book[field];
-        tr.appendChild(td);
+        //console.log(field);
+        switch (field) {
+            default:
+                
+                var td = document.createElement('td');
+                td.textContent = book[field];
+                //console.log(td);
+                tr.appendChild(td);
+                break;
+
+            case 'bSal':
+                
+                var money = book['bSal'];
+                //console.log(typeof money);
+                td.textContent = money.toLocaleString() + "원";
+                
+                //console.log(td);
+                tr.appendChild(td);
+                break;
+        }
     }
+
     //버튼
     td = document.createElement('td');
     var btn = document.createElement('button');
@@ -155,48 +172,45 @@ function getTrBook(book) {
     return tr;
 }
 
-// function changeFnc() {
-//     var searcBCode = this.parentNode.nextSibling.nextSibling.textContent;
-//     (this.parentNode.nextSibling.nextSibling.textContent); //.nextSibling.nextSibling 다음값을 불러올때 추가
-//     if (this.checked == true) {
-//         var lnth = checkedAry.length;
-//         // checkedAry[lnth] = searchSno;
-//         // checkedAry.splice(0, 0,searcBCode);//앞에 추가
-//         checkedAry.splice(lnth, 0, searcBCode); //뒤에 추가
-//     } else {
-//         for (var i = 0; i < checkedAry.length; i++) {
-//             if (checkedAry[i] == searcBCode) {
-//                 // delete checkedAry[i]; // 지운값은 null값이 됨
-//                 // checkedAry.splice(i, 1, 'added','two added'); //i위치에서 1의 크기를 뒤에값으로 대체하겠다.
-//                 checkedAry.splice(i, 1); //i위치에서 1의 크기를 대체값이 없으면 지움
-//                 // checkedAry.splice(i, 2); 
-//                 break;
-//             }
-//         }
-//     }
-//     console.log(checkedAry);
-// }
 
-function selDelete(e){
-    var isChk = false;
-    var book_chk = document.getElementsByName("chk");
-    console.log(book_chk);
-    for(var i=0;i<book_chk.length;i++){
-        if(book_chk[i].checked == true) {
-            isChk = true;
-            console.log();
-            break;
-        }
+
+function selDelete() {
+    var chek = document.querySelectorAll("input[type='checkbox']:checked");
+    // console.log(chek);
+    // console.log(chek.length);
+    // console.log(chek[0]);
+    // console.log(chek[0].parentElement.parentElement.remove());
+
+    for (var i = 0; i < chek.length; i++) {
+
+        chek[i].parentElement.parentElement.remove();
+
     }
+
 }
 
 function clickFnc(e) {
 
     // //화면에서 값 삭제
-    console.log(this.parentNode.parentNode.remove());
+    this.parentNode.parentNode.remove();
 
 }
 
+function selectAll(a) {
+  
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = a.checked;
+    })
+}
 
-
+function checkSelectAll(checkbox)  {
+    const selectall 
+      = document.querySelector('input[name="allChk"]');
+    
+    if(checkbox.checked === false)  {
+      selectall.checked = false;
+    }
+  }
+  
